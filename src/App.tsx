b58f0d038b8c9cb3c3c20b4035f9fec8a93d38b8
rezha4 <rezha.bahari@gmail.com>
2025-11-28 15:11:12 +0700

    working ver

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,79 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [originalLang, setOriginalLang] = useState("");
+  const [targetLang, setTargetLang] = useState("");
+  const [originalText, setOriginalText] = useState("");
+  const [targetText, setTargetText] = useState("");
   const worker = useRef<Worker | null>(null);
 
   useEffect(() => {
     if (!worker.current) {
-      worker.current = new Worker(
-        new URL("worker.js", import.meta.url),
-        {
-          type: "module",
-        }
-      );
+      worker.current = new Worker(new URL("worker.js", import.meta.url), {
+        type: "module",
+      });
     }
 
-    return () => {};
+    worker.current.addEventListener("message", (e) =>
+      setTargetText(JSON.stringify(e.data)),
+    );
+
+    const onMessageReceived = () => {};
+
+    return () => {
+      worker.current?.removeEventListener("message", onMessageReceived);
+    };
   }, []);
 
-  return <p>hello</p>;
+  const handleTranslate = () => {
+    worker.current?.postMessage({
+      text: originalText,
+      src_lang: originalLang,
+      tgt_lang: targetLang,
+    });
+  };
+
+  return (
+    <div className="p-4 w-1/2 mx-auto space-y-4 text-center">
+      <h1>translate with AI in your browser (transformers.js)</h1>
+
+      <div className="flex gap-4">
+        <div className="flex gap-2 flex-col w-full">
+          <label className="label" htmlFor="originalLang">
+            original language
+          </label>
+          <input
+            value={originalLang}
+            onChange={(e) => setOriginalLang(e.target.value)}
+            className="input"
+            id="originalLang"
+          />
+          <textarea
+            value={originalText}
+            onChange={(e) => setOriginalText(e.target.value)}
+            className="textarea h-52"
+          />
+        </div>
+
+        <div className="flex gap-2 flex-col w-full">
+          <label className="label" htmlFor="originalLang">
+            target language
+          </label>
+          <input
+            value={targetLang}
+            onChange={(e) => setTargetLang(e.target.value)}
+            className="input"
+            id="originalLang"
+          />
+          <textarea value={targetText} className="textarea h-52" disabled />
+        </div>
+      </div>
+
+      <button className="btn w-1/2" onClick={handleTranslate}>
+        translate!
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,29 @@
-console.log("worker is alive")
+import { pipeline } from "@huggingface/transformers";
+
+class Pipeline {
+  static model = "Xenova/nllb-200-distilled-600M";
+  static instance = null;
+
+  /**
+   * @param {string} text
+   * @param {Object} { src_lang: string, tgt_lang: string }
+   */
+  static async getInstance() {
+    this.instance ??= pipeline("translation", this.model);
+
+    return this.instance;
+  }
+}
+
+self.addEventListener("message", async (e) => {
+  const { text, src_lang, tgt_lang } = e.data;
+
+  const translator = await Pipeline.getInstance();
+
+  const output = await translator(text, {
+    src_lang,
+    tgt_lang,
+  });
+
+  self.postMessage(output);
+});
